6674b13631d81ba62342274264d1da9459cd20f8
Giovanni Ciotta <giociotta@gmail.com>
2023-02-08 16:55:05 +0100

    milestone 3

--- src/nations/Main.java
+++ src/nations/Main.java
@@ -20,18 +20,18 @@ public class Main {
 		try (Connection con = DriverManager.getConnection(url, user, password)) {
 
 			System.out.println("Connessione effettuata con successo ");
-//			  String email=s.nextLine();
-//			  System.out.print("Inserisci la tua password: ");
-//			  String pwd=s.nextLine();
+			System.out.print(" ricerca qulacosa: ");
+			String inputUser = s.nextLine();
 
+//			
 			String sql = "SELECT c.name , c.country_id,r.name, c2.name  FROM nations.countries c\n"
 					+ "INNER JOIN nations.regions r \n" + "ON c.region_id = r.region_id \n"
 					+ "INNER JOIN nations.continents c2 \n" + "ON r.continent_id =c2.continent_id \n"
-					+ "ORDER BY c.name ";
+					+ "WHERE c.name LIKE ?\r\n " + "ORDER BY c.name ";
 
 			try (PreparedStatement ps = con.prepareStatement(sql)) {
-				// ps.setString(1, email);
-				// ps.setString(2, pwd);
+
+				ps.setString(1, "%" + inputUser + "%");
 
 				try (ResultSet rs = ps.executeQuery()) {
 
@@ -40,7 +40,7 @@ public class Main {
 					while (rs.next()) {
 						System.out.println(
 
-								rs.getString(1) + "\t\t" + rs.getInt(2) + "\t\t" + rs.getString(3) + "\t\t\t"
+								rs.getString(1) + "\t\t" + rs.getInt(2) + "\t\t" + rs.getString(3) + "\t\t\t\t"
 										+ rs.getString(4));
 
 					}
